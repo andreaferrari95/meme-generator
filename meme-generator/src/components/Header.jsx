ed8031bf2d7f85faa7a048fd84da7e52166fd54c
Andrea Ferrari <andreaferraridev@gmail.com>
2024-01-24 17:49:46 +0100

    asd

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header--container">
+      <img src="../troll-face.png" alt="Troll Face" className="header--img" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--h4">React Course - Project 3</h4>
+    </header>
+  );
+}
