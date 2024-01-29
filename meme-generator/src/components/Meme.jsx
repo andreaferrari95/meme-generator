e0895bc396a1f92ecb591aa8d6860361b9ddcf0b
Andrea Ferrari <andreaferraridev@gmail.com>
2024-01-29 10:58:37 +0100

    asd

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,21 +1,35 @@
+import memesData from "../../memesData";
+import React from "react";
+
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
     <main>
-      <form className="form">
-        <div>
-          <label className="form--label">Top text</label>
-          <input type="text" placeholder="Shut up" className="form--input" />
-        </div>
-        <div>
-          <label className="form--label">Bottom text</label>
-          <input
-            type="text"
-            placeholder="and take my money"
-            className="form--input"
-          />
-        </div>
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }
